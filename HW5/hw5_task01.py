472712c5dd50665c28eb38528cbc99e385930baa
anraspv <raspopova2015@mail.ru>
2022-04-21 16:09:42 +1000

    Added homework5

---
+++ HW5/hw5_task01.py
@@ -0,0 +1,54 @@
+import datetime
+
+
+class Homework:
+    def __init__(self, text, deadline: datetime.timedelta):
+        self.text = text
+        self.deadline = deadline
+        self.created = datetime.datetime.today()
+
+    def is_active(self):  # the method checks if the time has not expired before the deadline
+        today = datetime.datetime.now()
+        return bool((today - self.created).days < self.deadline)
+
+
+class Student:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def do_homework(self, Homework):
+        if not Homework.is_active():
+            print("you're late")
+            return None
+        return Homework
+
+
+class Teacher:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def create_homework(self, text: str, deadline: datetime.timedelta.days = datetime.timedelta(days=1)):
+        hw = Homework(text, deadline)
+        return hw
+
+
+if __name__ == '__main__':
+    teacher = Teacher('Daniil', 'Shadrin')
+    student = Student('Roman', 'Petrov')
+    teacher.last_name  # Daniil
+    student.first_name  # Petrov
+
+    expired_homework = teacher.create_homework('Learn functions', 0)
+    expired_homework.created  # Example: 2019-05-26 16:44:30.688762
+    expired_homework.deadline  # 0:00:00
+    expired_homework.text  # 'Learn functions'
+
+    # create function from method and use it
+    create_homework_too = teacher.create_homework
+    oop_homework = create_homework_too('create 2 simple classes', 5)
+    oop_homework.deadline  # 5 days, 0:00:00
+
+    student.do_homework(oop_homework)
+    student.do_homework(expired_homework)
